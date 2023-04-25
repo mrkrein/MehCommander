997653aff6e4eb81e08d67a5e94a9cd2a85096af
The Body4 <bogdanzheltovskik@mail.ru>
2023-04-26 00:40:33 +0300

    Create First Window

--- MehCommander.cpp
+++ MehCommander.cpp
@@ -1,2 +1,44 @@
 #include <stdio.h>
 #include <Windows.h>
+
+LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+WNDCLASSW NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
+
+int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPWSTR lpCmdLine, int nShowCmd) {
+	WNDCLASSW SoftwareMainClass = NewWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(NULL, IDI_WINLOGO), L"MainWndClass", SoftwareMainProcedure);
+
+	if (!RegisterClassW(&SoftwareMainClass)) { return -1; }
+	MSG SoftwareMainMessage = { 0 };
+
+	CreateWindowW(L"MainWndClass", L"Первое окно на WINAPI xD", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 250, NULL, NULL, NULL, NULL);
+
+	while (GetMessage(&SoftwareMainMessage, NULL, NULL, NULL)) {
+		TranslateMessage(&SoftwareMainMessage);
+		DispatchMessageW(&SoftwareMainMessage);
+	}
+	return 0;
+}
+
+WNDCLASSW NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure) {
+	WNDCLASSW NWC = { 0 };
+
+	NWC.hIcon = Icon;
+	NWC.hCursor = Cursor;
+	NWC.hInstance = hInst;
+	NWC.lpszClassName = Name;
+	NWC.hbrBackground = BGColor;
+	NWC.lpfnWndProc = Procedure;
+
+	return NWC;
+}
+
+LRESULT CALLBACK SoftwareMainProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg) {
+	case WM_CREATE:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default: return DefWindowProc(hWnd, msg, wp, lp);
+	}
+}
